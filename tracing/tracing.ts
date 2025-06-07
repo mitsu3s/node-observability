8527ec52b7777034e2b9d7014aa70c0b41e3bb0b
mitsu3s <mitsu3s.dev@gmail.com>
2025-06-07 18:43:55 +0900

    Fix docker settings

--- tracing/tracing.ts
+++ tracing/tracing.ts
@@ -6,9 +6,16 @@ import {
   PeriodicExportingMetricReader,
   ConsoleMetricExporter,
 } from "@opentelemetry/sdk-metrics";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+
+// OTLP exporter pointing to local Jaeger container
+const traceExporter = new OTLPTraceExporter({
+  url: "http://localhost:4318/v1/traces", // Jaeger OTLP endpoint
+});
 
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  serviceName: "node-otel",
+  traceExporter: traceExporter,
   metricReader: new PeriodicExportingMetricReader({
     exporter: new ConsoleMetricExporter(),
   }),
